a0abd7c3d45936aabb8916375d2a4ed678105bbf
leenclair <leesu9317@gmail.com>
2024-05-12 18:07:18 +0900

    Add initial files and .gitignore

---
+++ src/test/java/mastering/github/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package mastering.github.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
